70600588bd0dcfae8dcbe00bf21e578975f1dc1d
vitor Borges <vitoroliveira005@gmail.com>
2023-08-10 16:34:59 -0300

    Added edit article function to admin

--- articles/articlesController.js
+++ articles/articlesController.js
@@ -55,4 +55,38 @@ router.post('/articles/delete', (req, res) => {
     }
 })
 
+router.get('/admin/articles/edit/:id', (req, res) => {
+    let id = req.params.id
+    Article.findByPk(id).then(article => {
+        if (article != undefined) {
+            Category.findAll().then(categories => {
+                res.render('admin/articles/edit', { categories: categories, article: article })
+            })
+        } else {
+            res.redirect('/')
+        }
+    }).catch(err => {
+        res.redirect('/')
+    })
+})
+
+router.post("/articles/update", (req, res) => {
+    let id = req.body.id
+    let title = req.body.title
+    let body = req.body.body
+    let category = req.body.category
+
+    Article.update({ title: title, body: body, categoryId: category, slug: slugify(title) }, {
+        where: {
+            id: id
+        }
+    })
+        .then(() => {
+            res.redirect('/admin/articles')
+        })
+        .catch(err => {
+            res.redirect('/admin/articles')
+        })
+})
+
 module.exports = router
