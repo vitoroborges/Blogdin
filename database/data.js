042c8837a1611adabefe4b218f857dbc7fd13564
vitor Borges <vitoroliveira005@gmail.com>
2023-08-11 17:18:39 -0300

    Add page logic

--- database/data.js
+++ database/data.js
@@ -3,7 +3,8 @@ const Sequelize = require("sequelize")
 const connection = new Sequelize('blogdin', 'root', 'C.oxinha4321', {
     host: "localhost",
     dialect: 'mysql',
-    timezone: "-03:00"
+    timezone: "-03:00",
+    logging: false
 })
 
 module.exports = connection
